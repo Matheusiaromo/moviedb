7c6c76079b35072ea0f1145f2dac088ba34e82dc
= <matheusiaromo@gmail.com>
2021-12-16 11:49:48 -0300

    Menu criado

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Header />
+
+
   </div>
 </template>
 
+<script>
+
+import Header from '@/components/Header.vue';
+//import Footer from '@components/Footer.vue';
+
+
+export default {
+
+components: {
+  Header
+}
+
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap');
+
+
+
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding-left: 40px;
+  padding-right: 40px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+:root {
+  --primary: #032541;
+
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <header>
+      <div class="container">
+          <div>
+            <nav>
+                <ul>
+                    <li><img src="@/assets/logo-header.svg"></li>
+                    <li><a href="">Filmes</a></li>
+                    <li><a href="">Séries</a></li>
+                    <li><a href="">Pessoas</a></li>
+                    <li><a href="">Mais</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">+</a></li>
+                    <li><a href="">PT</a></li>
+                    <li><a href="">Iniciar Sessão</a></li>
+                    <li><a href="">Aderir ao TMDB</a></li>
+                    <li><a href="">Search</a></li>                        
+                </ul>
+            </nav>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+header {
+    background: var(--primary);
+    color: #fff;
+    font-size: 16px;
+    font-weight: 600;
+    font-family: 'Source Sans Pro', sans-serif;
+}
+
+
+
+nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 70px    ;
+}
+
+a {
+    text-decoration: none;
+    color: #fff;
+}
+
+ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+}
+
+li {
+    display: flex;
+}
+
+img {
+    height: 20px;
+}
+
+header ul:nth-child(1) li {
+    margin-right: 20px;
+}
+
+header ul:nth-child(2) li {
+    margin-left: 20px;
+}
+
+
+
+</style>
