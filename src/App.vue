922d275fa65e6acad4c9215d7ad7de3e63339767
= <matheusiaromo@gmail.com>
2021-12-17 00:34:44 -0300

    comecei o primeiro banner

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,10 @@ components: {
   box-sizing: border-box;
 }
 
+html, body {
+  font-family: 'Source Sans Pro', sans-serif;
+}
+
 .container {
   max-width: 1280px;
   margin: 0 auto;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
+    <div class="container">
+      <div class="hero">
+        <h1>Bem-vindo(a).</h1>
+        <h4>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h4>
+      </div>
+      <div class="search">
+        <input type="text" placeholder="Pesquise">
+        <input type="button" value="search">
+      </div>
+    </div>
     
-    <h1>Aqui vai a home</h1>
    
   </div>
 </template>
@@ -17,3 +26,30 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+.home {
+  color: #fff;
+}
+
+.container {
+  background: var(--primary);
+  padding-top: 40px;
+  padding-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 40px;
+}
+
+.hero h1 {
+  font-size: 48px;
+}
+
+.hero h4 {
+  font-size: 32px;
+}
+  
+
+</style>
